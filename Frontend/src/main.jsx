fe58d1a19fdb685401ff7dd0d45a1f3c43943f32
dev-manish301 <rajzmanish94@gmail.com>
2025-09-08 22:22:35 +0530

    creation for moblie version

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { registerSW } from 'virtual:pwa-register'
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "./components/AuthContext.jsx";
+
+
+registerSW()
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'vite-plugin-tailwindcss'; // Correct import for Tailwind
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [  tailwindcss(),react()],
-})
+  plugins: [
+    tailwindcss(), 
+    react(),
+    VitePWA({
+      name: "own-ai",
+      short_name: "own",
+      description: "ask question on own ai",
+      theme_color: "#0A47D5",
+      icons: [
+        { "src": "icons/icon-48x48.png", "sizes": "48x48", "type": "image/png" },
+        { "src": "icons/icon-72x72.png", "sizes": "72x72", "type": "image/png" },
+        { "src": "icons/icon-96x96.png", "sizes": "96x96", "type": "image/png" },
+        { "src": "icons/icon-128x128.png", "sizes": "128x128", "type": "image/png" },
+        { "src": "icons/icon-144x144.png", "sizes": "144x144", "type": "image/png" },
+        { "src": "icons/icon-152x152.png", "sizes": "152x152", "type": "image/png" },
+        { "src": "icons/icon-192x192.png", "sizes": "192x192", "type": "image/png" },
+        { "src": "icons/icon-256x256.png", "sizes": "256x256", "type": "image/png" },
+        { "src": "icons/icon-384x384.png", "sizes": "384x384", "type": "image/png" },
+        { "src": "icons/icon-512x512.png", "sizes": "512x512", "type": "image/png" }
+      ],
+      start_url: ".",
+      display: "standalone",
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /.*\.(?:png|jpg|jpeg|svg|gif)/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+              expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 }
+            }
+          },
+          {
+            urlPattern: /.*\.(?:json|js|css)/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'assets-cache',
+              expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 }
+            }
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+    })
+  ],
+});
