c5606b9725efe0c915805fa69f412d68f107e711
dev-manish301 <rajzmanish94@gmail.com>
2025-09-08 22:34:15 +0530

    Ready for production

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'vite-plugin-tailwindcss'; // Correct import for Tailwind
+import tailwindcss from '@tailwindcss/vite'// Correct import for Tailwind
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
